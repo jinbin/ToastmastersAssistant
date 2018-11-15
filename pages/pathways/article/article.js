7288b06cb1734ac81af5235415173d24acfbc5f8
jinbin <jinbin@weidian.com>
2018-11-15 20:50:35 +0800

    修改article样式

---
+++ pages/pathways/article/article.js
@@ -0,0 +1,65 @@
+//article.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  },
+
+  onLoad: function (options) {
+    let name_en
+    let name_cn
+    console.log(options.level)
+
+    if (options.level == 7) {
+      name_en = "Pathways资料"
+      name_cn = "Pathways Resources"
+    }
+    
+    this.setData({
+      level_name_en: name_en,
+      level_name_ch: name_cn
+    })
+
+    if (options.level == 7) {
+      this.setData({
+        level: app.level7
+      })
+    }
+  },
+
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  },
+
+  copyText: function (e) {
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.text,
+      success: function (res) {
+        wx.showToast({
+          title: "内容已复制"
+        })
+      }
+    })
+  },
+
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, level = this.data.level;
+    for (var i = 0, len = level.length; i < len; ++i) {
+      if (level[i].id == id) {
+        level[i].open = !level[i].open
+      } else {
+        level[i].open = false
+      }
+    }
+    this.setData({
+      level: level
+    });
+  },
+
+  onShareAppMessage: function (options) {
+  }
+})
--- pages/pathways/levels/levels.js
+++ pages/pathways/levels/levels.js
@@ -10,12 +10,21 @@ Page({
   },
 
   toLevel: function (options) {
-    wx.navigateTo({
-      url: '/pages/pathways/desc/desc?level=' + options.currentTarget.id,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
+    if (options.currentTarget.id != 7){
+      wx.navigateTo({
+        url: '/pages/pathways/desc/desc?level=' + options.currentTarget.id,
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
+      })
+    } else if (options.currentTarget.id == 7){
+      wx.navigateTo({
+        url: '/pages/pathways/article/article?level=' + options.currentTarget.id,
+        success: function (res) { },
+        fail: function (res) { },
+        complete: function (res) { },
+      })     
+    }
   },
 
   onShareAppMessage: function () {
