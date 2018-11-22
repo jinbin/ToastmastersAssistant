578e0048e662d98d30a0abab7036a0d55a2be051
jinbin <jinbin@weidian.com>
2018-11-22 15:59:18 +0800

    commit

--- pages/pathways/levels/levels.js
+++ pages/pathways/levels/levels.js
@@ -2,6 +2,10 @@
 
 const app = getApp()
 
+const db = wx.cloud.database({
+  env: "tmassistant-5275ad"
+})
+
 Page({
   data: {
   },
@@ -9,21 +13,37 @@ Page({
   onLoad: function (options) {
   },
 
-  toLevel: function (options) {
-    if (options.currentTarget.id != 7){
+  levelHandler: function (options) {
+    console.log(options.detail.target.id)
+    console.log(options.detail.formId)
+
+    var timestamp = Date.parse(new Date()) / 1000
+    var newtimestamp = timestamp + 24 * 60 * 60 * 7
+    var n7_to = newtimestamp * 1000
+
+    db.collection("formIds").add({
+      data: {
+        openid: this.data.openid,
+        formId: options.detail.formId,
+        expire: new Date(n7_to),
+        available: true
+      }
+    })
+
+    if (options.detail.target.id != 7) {
       wx.navigateTo({
-        url: '/pages/pathways/desc/desc?level=' + options.currentTarget.id,
-        success: function(res) {},
-        fail: function(res) {},
-        complete: function(res) {},
-      })
-    } else if (options.currentTarget.id == 7){
-      wx.navigateTo({
-        url: '/pages/pathways/article/article?level=' + options.currentTarget.id,
+        url: '/pages/pathways/desc/desc?level=' + options.detail.target.id,
         success: function (res) { },
         fail: function (res) { },
         complete: function (res) { },
-      })     
+      })
+    } else if (options.detail.target.id == 7) {
+      wx.navigateTo({
+        url: '/pages/pathways/article/article?level=' + options.detail.target.id,
+        success: function (res) { },
+        fail: function (res) { },
+        complete: function (res) { },
+      })
     }
   },
 
--- pages/tm/clock/set/set.js
+++ pages/tm/clock/set/set.js
@@ -1,8 +1,37 @@
 
 const app = getApp()
 
+const db = wx.cloud.database({
+  env: "tmassistant-5275ad"
+})
+
 Page({
   data: {},
+
+  timeHandler: function (options) {
+
+    var timestamp = Date.parse(new Date()) / 1000
+    var newtimestamp = timestamp + 24 * 60 * 60 * 7
+    var n7_to = newtimestamp * 1000
+
+    db.collection("formIds").add({
+      data: {
+        openid: this.data.openid,
+        formId: options.detail.formId,
+        expire: new Date(n7_to),
+        available: true
+      }
+    })
+
+    wx.navigateTo({
+      url: '/pages/tm/clock/countdown/countdown?time=' + options.detail.target.id,
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+
+
+  },
 
   countdown: function (e) {
     console.log(e.currentTarget.id)
@@ -13,6 +42,7 @@ Page({
       complete: function(res) {},
     })
   },
+
   onShareAppMessage: function () {
   }
 })
